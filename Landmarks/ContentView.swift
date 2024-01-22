3fb397a97dd4d28f5b9956c9022ed1835552479e
stardvst <lilia.manukyan@hotmail.com>
2024-01-22 22:32:30 +0400

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,19 @@
+// Copyright © 2024 Landmarks. All rights reserved.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,12 @@
+// Copyright © 2024 Landmarks. All rights reserved.
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
