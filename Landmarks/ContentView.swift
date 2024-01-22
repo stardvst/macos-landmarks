e43475a8e4a7418a1d30f8191731be401869ead6
stardvst <lilia.manukyan@hotmail.com>
2024-01-22 22:37:23 +0400

    changed text and font

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -4,13 +4,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Turtle Rock")
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
     }
 }
 
