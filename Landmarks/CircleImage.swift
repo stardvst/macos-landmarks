b4ad592d27258ccb845affdf3b00f296981a1c48
stardvst <lilia.manukyan@hotmail.com>
2024-02-14 22:59:44 +0400

    added circled image

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,13 @@
+// Copyright © 2024 Landmarks. All rights reserved.
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
