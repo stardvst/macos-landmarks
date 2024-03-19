ccba780de6fb37880518f0e1642d42deac07e861
stardvst <lilia.manukyan@hotmail.com>
2024-03-19 21:19:03 +0400

    added map view

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,21 @@
+// Copyright © 2024 Landmarks. All rights reserved.
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+    
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+#Preview {
+    MapView()
+}
