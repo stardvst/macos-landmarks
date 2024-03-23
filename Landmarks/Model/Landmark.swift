3f7271b4f2b3f53bdfe1af021d4366c9498cd276
stardvst <lilia.manukyan@hotmail.com>
2024-03-23 15:02:56 +0400

    added landmark model

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,28 @@
+// Copyright © 2024 Landmarks. All rights reserved.
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    private var coordinates: Coordinates
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
+}
