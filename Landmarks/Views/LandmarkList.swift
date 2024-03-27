f80146b336f763551ceed982e33050e5483ebdd6
stardvst <lilia.manukyan@hotmail.com>
2024-03-27 22:40:16 +0400

    display landmarks in list structure #2

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -1,3 +1,17 @@
 // Copyright © 2024 Landmarks. All rights reserved.
 
 import Foundation
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
