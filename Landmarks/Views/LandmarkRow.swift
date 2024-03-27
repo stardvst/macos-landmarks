44b8269018760d69f23799ef3a983195096c607f
stardvst <lilia.manukyan@hotmail.com>
2024-03-27 20:51:13 +0400

    added a landmark row ui

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+// Copyright © 2024 Landmarks. All rights reserved.
+
+import Foundation
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image.resizable().frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
