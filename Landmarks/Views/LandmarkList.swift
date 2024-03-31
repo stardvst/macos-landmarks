5b0ab1dc6e73a7f46a280a568a92848bba2b7730
stardvst <lilia.manukyan@hotmail.com>
2024-03-31 16:12:54 +0400

    added control to toggle favorites

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -4,7 +4,7 @@ import Foundation
 import SwiftUI
 
 struct LandmarkList: View {
-    @State private var showFavoritesOnly = true
+    @State private var showFavoritesOnly = false
     
     var filteredLandmarks: [Landmark] {
         landmarks.filter { landmark in
@@ -14,14 +14,20 @@ struct LandmarkList: View {
     
     var body: some View {
         NavigationSplitView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
                 }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+                .animation(.default, value: filteredLandmarks)
+                .navigationTitle("Landmarks")
             }
-            .navigationTitle("Landmarks")
         } detail: {
             Text("Select a landmark")
         }
