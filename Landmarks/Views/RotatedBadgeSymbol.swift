daff9b56966457f297584d20425988a7c2d75289
stardvst <lilia.manukyan@hotmail.com>
2024-04-06 15:16:26 +0400

    drawing the badge symbol

---
+++ Landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,18 @@
+// Copyright © 2024 Landmarks. All rights reserved.
+
+import Foundation
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
