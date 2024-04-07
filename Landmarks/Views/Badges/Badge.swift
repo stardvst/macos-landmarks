cb0088638abd180ef66746e1bed7755ff7a8eb97
stardvst <lilia.manukyan@hotmail.com>
2024-04-07 14:11:39 +0400

    created the badge symbol class

---
+++ Landmarks/Views/Badges/Badge.swift
@@ -0,0 +1,32 @@
+// Copyright © 2024 Landmarks. All rights reserved.
+
+import Foundation
+import SwiftUI
+
+struct Badge: View {
+    var badgeSymbols: some View {
+        ForEach(0..<8) { index in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(index) / Double(8)) * 360.0
+            )
+        }
+        .opacity(0.5)
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
+        }
+        .scaledToFit()
+    }
+}
+
+#Preview {
+    Badge()
+}
--- Landmarks/Views/Badges/BadgeBackground.swift
+++ Landmarks/Views/Badges/BadgeBackground.swift
@@ -7,19 +7,17 @@ struct BadgeBackground: View {
     var body: some View {
         GeometryReader { geometry in
             Path { path in
-                var width: CGFloat = 100.0
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
                 let height = width
                 let xScale: CGFloat = 0.832
                 let xOffset = (width * (1.0 - xScale)) / 2.0
                 width *= xScale
-
                 path.move(
                     to: CGPoint(
                         x: width * 0.95 + xOffset,
                         y: height * (0.20 + HexagonParameters.adjustment)
                     )
                 )
-                
                 HexagonParameters.segments.forEach { segment in
                     path.addLine(
                         to: CGPoint(
