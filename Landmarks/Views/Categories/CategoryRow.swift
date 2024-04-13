f86d3182f5841cf30b3c1c1108a20c0ca2220bf0
stardvst <lilia.manukyan@hotmail.com>
2024-04-13 15:15:37 +0400

    added category item and row

---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,35 @@
+// Copyright © 2024 Landmarks. All rights reserved.
+
+import Foundation
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 15)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+#Preview {
+    let landmarks = ModelData().landmarks
+    return CategoryRow(
+        categoryName: landmarks[0].category.rawValue,
+        items: Array(landmarks.prefix(4))
+    )
+}
