5033d549dc3bc4955b1f14e8d298eec95308c9fc
stardvst <lilia.manukyan@hotmail.com>
2024-05-01 15:32:07 +0400

    added watchos target

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,12 @@
+// Copyright © 2024 Landmarks. All rights reserved.
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
